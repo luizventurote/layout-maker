7bede89754412e23fc7baa578aea30ef1584be80
luizventurote <luiz_17@ymail.com>
2014-04-28 23:44:53 -0300

    Method copyFile removed from class Arquivo.

--- layout-maker-project/src/model/Arquivo.java
+++ layout-maker-project/src/model/Arquivo.java
@@ -1,13 +1,7 @@
 package model;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.channels.FileChannel;
-
 public class Arquivo {
-    
+
     private int id;
     private String nome;
     private String extensao;
@@ -15,9 +9,9 @@ public class Arquivo {
     public Arquivo(int id, String nome_base, String extensao) {
         this.id = id;
         this.extensao = extensao;
-        
+
         // Monta o nome do arquivo
-        this.nome = Integer.toString(id)+'_'+nome_base+'.'+extensao;
+        this.nome = Integer.toString(id) + '_' + nome_base + '.' + extensao;
     }
 
     public String getExtensao() {
@@ -43,26 +37,5 @@ public class Arquivo {
     public void setNome(String nome) {
         this.nome = nome;
     }
-    
-    
-     public static void copyFile(File source, File destination) throws IOException {  
-     if (destination.exists())  
-         destination.delete();  
-  
-     FileChannel sourceChannel = null;  
-     FileChannel destinationChannel = null;  
-  
-     try {  
-         sourceChannel = new FileInputStream(source).getChannel();  
-         destinationChannel = new FileOutputStream(destination).getChannel();  
-         sourceChannel.transferTo(0, sourceChannel.size(),  
-                 destinationChannel);  
-     } finally {  
-         if (sourceChannel != null && sourceChannel.isOpen())  
-             sourceChannel.close();  
-         if (destinationChannel != null && destinationChannel.isOpen())  
-             destinationChannel.close();  
-    }  
-}  
-    
+
 }
