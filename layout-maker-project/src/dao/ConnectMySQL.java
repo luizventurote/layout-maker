fd603ca868ff1c03ea7f275a849c6a66884de219
luizventurote <luiz_17@ymail.com>
2014-05-02 16:04:25 -0300

    ConnecMySQL class added.

---
+++ layout-maker-project/src/dao/ConnectMySQL.java
@@ -0,0 +1,23 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectMySQL {
+    
+    static Connection db_connect;
+    static String server = "127.0.0.1";  // localhost
+    static String database = "jeanxfome";
+    static String login = "root";
+    static String pass = "root";
+    
+    static Connection startConnection() throws Exception, SQLException {
+        
+        String url = "jdbc:mysql://" + server + "/" + database ;
+        
+        Class.forName("com.mysql.jdbc.Driver");
+        db_connect = DriverManager.getConnection(url, login, pass);
+        return db_connect;                
+    }
+}
