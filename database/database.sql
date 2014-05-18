ca9a0311149606fa364a2634ff7cfa283f33e6d3
luizventurote <luiz_17@ymail.com>
2014-05-18 15:06:21 -0300

    Script SQL added.

---
+++ database/database.sql
@@ -0,0 +1,12 @@
+CREATE SCHEMA IF NOT EXISTS layout_maker;
+USE layout_maker;
+
+-- -----------------------------------------------------
+-- Table lm_file
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS lm_file (
+  id_file INT(11) NOT NULL AUTO_INCREMENT,
+  name VARCHAR(45) NOT NULL,
+  extension CHAR(5) NOT NULL,
+  PRIMARY KEY (id_file)
+);
