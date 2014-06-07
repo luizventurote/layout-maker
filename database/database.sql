cc1cdf98e6749df54fb5c3c262833d8899149af5
luizventurote <luiz_17@ymail.com>
2014-06-07 01:21:59 -0300

    Table component added.

--- database/database.sql
+++ database/database.sql
@@ -1,24 +1,54 @@
 CREATE SCHEMA IF NOT EXISTS layout_maker;
 USE layout_maker;
 
+
 -- -----------------------------------------------------
 -- Table config
 -- -----------------------------------------------------
-CREATE TABLE config (
+CREATE TABLE IF NOT EXISTS config (
   id_config INT NOT NULL,
   path_files VARCHAR(50) NULL,
-  PRIMARY KEY (id_config)
-);
+  PRIMARY KEY (id_config))
+ENGINE = InnoDB;
 
-INSERT INTO config (id_config, path_files) VALUES ('1', 'files/files/');
 
 -- -----------------------------------------------------
--- Table lm_file
+-- Table arquivo
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS arquivo (
-  id INT(11) NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(45) NOT NULL,
-  extension CHAR(5) NOT NULL,
+  extension VARCHAR(45) NOT NULL,
   directory VARCHAR(45) NOT NULL,
-  PRIMARY KEY (id)
-);
+  PRIMARY KEY (id))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table componente
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS componente (
+  id INT NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table componente_arquivo
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS componente_arquivo (
+  componente_id INT NOT NULL,
+  arquivo_id INT NOT NULL,
+  PRIMARY KEY (componente_id, arquivo_id),
+  CONSTRAINT fk_componente_has_arquivo_componente
+    FOREIGN KEY (componente_id)
+    REFERENCES componente (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_componente_has_arquivo_arquivo1
+    FOREIGN KEY (arquivo_id)
+    REFERENCES arquivo (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
