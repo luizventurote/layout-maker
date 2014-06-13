31472114b2284a8ef992056023084d963abbe80e
luizventurote <luiz_17@ymail.com>
2014-06-13 18:39:47 -0300

    Report control added.

---
+++ layout-maker-project/src/control/ReportControl.java
@@ -0,0 +1,26 @@
+package control;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import model.Arquivo;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.view.JasperViewer;
+import static org.hibernate.cfg.AvailableSettings.URL;
+import util.HibernateUtil;
+
+public class ReportControl {
+
+    public void showArquivoReport() throws Exception {
+        
+           
+
+    }
+
+}
