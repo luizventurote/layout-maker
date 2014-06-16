535c15be62553f84b08594d7e4ee1e8ef50bee24
luizventurote <luiz_17@ymail.com>
2014-06-16 01:07:51 -0300

    Category Dao added.

---
+++ layout-maker-project/src/dao/CategoryDao.java
@@ -0,0 +1,72 @@
+package dao;
+
+import java.sql.SQLException;
+import model.Categoria;
+import org.hibernate.HibernateException;
+
+public class CategoryDao extends DefaultDao {
+    
+    public void insert(Categoria cat) throws Exception, SQLException, HibernateException {
+
+        try {
+
+            this.session = util.HibernateUtil.getSessionFactory().openSession();
+
+            this.session.beginTransaction();
+
+            this.session.save(cat);
+
+            this.session.getTransaction().commit();
+
+        } catch (HibernateException he) {
+            session.getTransaction().rollback();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    public void delete(Categoria cat) throws Exception, SQLException, HibernateException {
+
+        try {
+
+            this.session = util.HibernateUtil.getSessionFactory().openSession();
+
+            this.session.beginTransaction();
+
+            this.session.delete(cat);
+
+            this.session.getTransaction().commit();
+
+        } catch (HibernateException he) {
+            session.getTransaction().rollback();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    public void update(Categoria cat) throws Exception, SQLException, HibernateException {
+
+        try {
+
+            this.session = util.HibernateUtil.getSessionFactory().openSession();
+
+            this.session.beginTransaction();
+
+            this.session.update(cat);
+
+            this.session.getTransaction().commit();
+
+        } catch (HibernateException he) {
+            session.getTransaction().rollback();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+    
+}
