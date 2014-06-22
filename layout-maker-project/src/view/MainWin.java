91699e3a1e8c27c175a581a78f0764b4c73e79b0
luizventurote <luiz_17@ymail.com>
2014-06-21 21:20:11 -0300

    New release.

--- layout-maker-project/src/view/MainWin.java
+++ layout-maker-project/src/view/MainWin.java
@@ -113,7 +113,7 @@ public class MainWin extends javax.swing.JFrame {
             }
         });
 
-        jLabel1.setText("v0.0.9");
+        jLabel1.setText("v0.1.0");
 
         jLabel2.setText("By Luiz Venturote");
 
